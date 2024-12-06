b9b2fcde975568ca2e3248082e9e6789dbb135b4
ankitojha07 <ankitojha1409@gmail.com>
2024-12-07 01:54:21 +0530

    sorted issues in image importing

--- components/ImageCard.tsx
+++ components/ImageCard.tsx
@@ -1,63 +1,20 @@
 import React from 'react';
 import {Image, ScrollView, StyleSheet, Text, View} from 'react-native';
 
-import image1 from '../images/image.jpg';
-import image2 from '../images/image-1.jpg';
-import image3 from '../images/image-2.jpg';
-import image4 from '../images/image-3.jpg';
-import image5 from '../images/image-4.jpg';
-import image6 from '../images/image-5.jpg';
-import image7 from '../images/image-6.jpg';
-import image8 from '../images/image-7.jpg';
-import image9 from '../images/image-8.jpg';
-import image10 from '../images/image-9.jpg';
+import {images} from '../utilities/image';
 
 const ImageCard = () => {
   return (
-    <View style={{padding: 8}}>
+    <View style={style.basic}>
       <Text style={style.headingText}>Image View</Text>
       <ScrollView horizontal={true}>
         <View style={style.container}>
-          <View style={style.imgCard}>
-            <Image source={image1} style={style.img} />
-            <Text>Mahal</Text>
-          </View>
-          <View style={style.imgCard}>
-            <Image source={image2} style={style.img} />
-            <Text>Image Text</Text>
-          </View>
-          <View style={style.imgCard}>
-            <Image source={image3} style={style.img} />
-            <Text>Image Text</Text>
-          </View>
-          <View style={style.imgCard}>
-            <Image source={image4} style={style.img} />
-            <Text>Image Text</Text>
-          </View>
-          <View style={style.imgCard}>
-            <Image source={image5} style={style.img} />
-            <Text>Image Text</Text>
-          </View>
-          <View style={style.imgCard}>
-            <Image source={image6} style={style.img} />
-            <Text>Image Text</Text>
-          </View>
-          <View style={style.imgCard}>
-            <Image source={image7} style={style.img} />
-            <Text>Image Text</Text>
-          </View>
-          <View style={style.imgCard}>
-            <Image source={image8} style={style.img} />
-            <Text>Image Text</Text>
-          </View>
-          <View style={style.imgCard}>
-            <Image source={image9} style={style.img} />
-            <Text>Image Text</Text>
-          </View>
-          <View style={style.imgCard}>
-            <Image source={image10} style={style.img} />
-            <Text>Image Text</Text>
-          </View>
+          {images.map((img, index) => (
+            <View style={style.imgCard}>
+              <Image key={index} source={img} style={style.img} />
+              <Text>Image Text</Text>
+            </View>
+          ))}
         </View>
       </ScrollView>
     </View>
@@ -65,6 +22,9 @@ const ImageCard = () => {
 };
 
 const style = StyleSheet.create({
+  basic: {
+    padding: 8,
+  },
   headingText: {
     color: '#fff',
     fontSize: 24,
@@ -90,6 +50,7 @@ const style = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: '#fff',
     borderRadius: 4,
+    gap: 2,
   },
 });
 
---
+++ utilities/image.js
@@ -0,0 +1,13 @@
+// images.js
+export const images = [
+    require('../images/image-1.jpg'),
+    require('../images/image-2.jpg'),
+    require('../images/image-3.jpg'),
+    require('../images/image-4.jpg'),
+    require('../images/image-5.jpg'),
+    require('../images/image-6.jpg'),
+    require('../images/image-7.jpg'),
+    require('../images/image-8.jpg'),
+    require('../images/image-9.jpg'),
+    require('../images/image-10.jpg'),
+];
